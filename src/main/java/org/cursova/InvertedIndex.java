1084901a2162fb394c7b8c902e542b7fb0a32485
Volodymyr Tsupryk <lordvolodua@gmail.com>
2024-01-03 17:24:50 +0000

    -add multithreading for adding documents to InvertedIndex

--- src/main/java/org/cursova/InvertedIndex.java
+++ src/main/java/org/cursova/InvertedIndex.java
@@ -1,17 +1,18 @@
 package org.cursova;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class InvertedIndex {
-    private Map<String, Set<String>> index;
+    private static final int NUMBER_OF_THREADS = 10;
+    private static Map<String, Set<String>> index;
 
     public InvertedIndex() {
-        this.index = new HashMap<>();
+        index = new ConcurrentHashMap<>();
     }
 
     public void addDocument(String fileName, String content) {
@@ -22,9 +23,48 @@ public class InvertedIndex {
                         .add(fileName));
     }
 
+    public void addDocuments(Map<String, String> fileTextMap, String[] fileNames) {
+        DocumentAdder[] documentAdder = new DocumentAdder[NUMBER_OF_THREADS];
+        Thread[] thread = new Thread[NUMBER_OF_THREADS];
+        for(int i = 1; i <= NUMBER_OF_THREADS; i++) {
+            thread[i - 1] = new Thread(documentAdder[i - 1]
+                    = new DocumentAdder(fileTextMap, Arrays.copyOfRange(fileNames,
+                    fileNames.length / NUMBER_OF_THREADS * (i - 1),
+                    fileNames.length / NUMBER_OF_THREADS * i)));
+            thread[i - 1].start();
+        }
+        for(int i = 0; i < NUMBER_OF_THREADS; i++) {
+            try {
+                thread[i].join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
     public List<String> search(String word) {
-        return index.getOrDefault(word, new HashSet<>())
-                .stream()
-                .toList();
+        Set<String> resultSet = new HashSet<>(index.getOrDefault(word, new HashSet<>()));
+        return resultSet.stream().toList();
+    }
+
+    private static class DocumentAdder implements Runnable {
+        private final Map<String, String> fileTextMap;
+        private final String[] fileNames;
+
+        public DocumentAdder(Map<String, String> fileTextMap, String[] fileNames) {
+            this.fileTextMap = fileTextMap;
+            this.fileNames = fileNames;
+        }
+
+        @Override
+        public void run() {
+            for(String fileName : fileNames) {
+                String[] words = fileTextMap.get(fileName).toLowerCase().split("\\W+");
+                Arrays.stream(words)
+                        .forEach(w -> index
+                                .computeIfAbsent(w, k -> new HashSet<>())
+                                .add(fileName));
+            }
+        }
     }
 }
--- src/main/java/org/cursova/Main.java
+++ src/main/java/org/cursova/Main.java
@@ -9,17 +9,9 @@ public class Main {
         FileService fileReader = new FileService();
         String[] filesInDirectory = fileReader.showFilesInDirectory("C:\\kpi\\cursova\\train\\neg\\");
 
+
         Map<String, String> fileTextMap = fileReader.readFiles(filesInDirectory);
-
-
-        System.out.println(filesInDirectory[0]);
-        System.out.println(filesInDirectory[1]);
-
-        String s = fileReader.readFile(filesInDirectory[0]);
-        invertedIndex.addDocument(filesInDirectory[0], s);
-
-        s = fileReader.readFile(filesInDirectory[1]);
-        invertedIndex.addDocument(filesInDirectory[1], s);
+        invertedIndex.addDocuments(fileTextMap, filesInDirectory);
 
         System.out.println("Search results for the 'say':"
                 + invertedIndex.search("say"));
